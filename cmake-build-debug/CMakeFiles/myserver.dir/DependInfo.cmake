3e0302d4957c98b94833f5cc60c96c9aea0e638f
suxiaolin <dear.lin@live.com>
2019-08-09 13:27:07 +0800

    step 1

---
+++ cmake-build-debug/CMakeFiles/myserver.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/suxiaolin/Dropbox/Projects/redis-4.0.1/echodemo/myserver.c" "/Users/suxiaolin/Dropbox/Projects/redis-4.0.1/cmake-build-debug/CMakeFiles/myserver.dir/echodemo/myserver.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/suxiaolin/Dropbox/Projects/redis-4.0.1/cmake-build-debug/CMakeFiles/ae.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/myserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myserver.dir/echodemo/myserver.c.o"
+  "myserver.pdb"
+  "myserver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
